271005cc791aa652065f93e75d8f52e81bca0414
Shon0221 <yoy285437@gmal.com>
2023-02-24 18:43:36 +0800

    Add dynamicArray

--- data_structure/golang/array/main.go
+++ data_structure/golang/array/main.go
@@ -5,7 +5,19 @@ import "fmt"
 func main() {
 	arr := []int32{1, 2, 3, 4, 5}
 	revArr := reverseArray(arr)
-	fmt.Println(revArr)
+	fmt.Println("reverseArray", revArr)
+
+	queries := [][]int32{
+		{1, 0, 5},
+		{1, 1, 7},
+		{1, 0, 3},
+		{2, 1, 0},
+		{2, 1, 1},
+	}
+	n := int32(2)
+
+	result := dynamicArray(n, queries)
+	fmt.Println("dynamicArray", result) // [7 3]
 }
 
 func reverseArray(a []int32) []int32 {
@@ -15,4 +27,27 @@ func reverseArray(a []int32) []int32 {
 		a[i], a[j] = a[j], a[i]
 	}
 	return a
+}
+
+func dynamicArray(n int32, queries [][]int32) []int32 {
+	seqList := make([][]int32, n)
+	lastAns := int32(0)
+	results := make([]int32, 0)
+
+	for _, q := range queries {
+		t := q[0]
+		x := q[1]
+		y := q[2]
+		idx := (x ^ lastAns) % n
+
+		if t == 1 {
+			seqList[idx] = append(seqList[idx], y)
+		} else if t == 2 {
+			size := int32(len(seqList[idx]))
+			lastAns = seqList[idx][y%size]
+			results = append(results, lastAns)
+		}
+	}
+
+	return results
 }
