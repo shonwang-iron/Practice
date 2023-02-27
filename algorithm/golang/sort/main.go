fd127a1ae1e6d6dfeafb85c69c3797ce75ca0f59
shonwang-iron <yoy285437@gmal.com>
2023-02-27 17:05:21 +0800

    Add heap sort

--- algorithm/golang/sort/main.go
+++ algorithm/golang/sort/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func main() {
+	arr := []int{5, 2, 4, 6, 1, 3}
+	fmt.Println("Before insertion sorting:", arr)
+	insertionSort(arr)
+	fmt.Println("After insertion sorting:", arr)
+
+	arr = []int{12, 11, 13, 5, 6, 7}
+	fmt.Println("Before heap sorting:", arr)
+	heapSort(arr)
+	fmt.Println("After heap sorting:", arr)
+}
+
 func insertionSort(arr []int) {
 	for i := 1; i < len(arr); i++ {
 		key := arr[i]
@@ -14,9 +26,34 @@ func insertionSort(arr []int) {
 	}
 }
 
-func main() {
-	arr := []int{5, 2, 4, 6, 1, 3}
-	fmt.Println("Before sorting:", arr)
-	insertionSort(arr)
-	fmt.Println("After sorting:", arr)
+func heapify(arr []int, n, i int) {
+	largest := i
+	left := 2*i + 1
+	right := 2*i + 2
+
+	if left < n && arr[left] > arr[largest] {
+		largest = left
+	}
+
+	if right < n && arr[right] > arr[largest] {
+		largest = right
+	}
+
+	if largest != i {
+		arr[i], arr[largest] = arr[largest], arr[i]
+		heapify(arr, n, largest)
+	}
+}
+
+func heapSort(arr []int) {
+	n := len(arr)
+
+	for i := n/2 - 1; i >= 0; i-- {
+		heapify(arr, n, i)
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		arr[0], arr[i] = arr[i], arr[0]
+		heapify(arr, i, 0)
+	}
 }
