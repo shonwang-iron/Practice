745edb6452d19e8088f540a988f35fe4cca7b413
shonwang-iron <yoy285437@gmal.com>
2023-02-27 17:13:06 +0800

    Add merge sort

--- algorithm/golang/sort/main.go
+++ algorithm/golang/sort/main.go
@@ -12,6 +12,11 @@ func main() {
 	fmt.Println("Before heap sorting:", arr)
 	heapSort(arr)
 	fmt.Println("After heap sorting:", arr)
+
+	arr = []int{5, 2, 4, 6, 1, 3}
+	fmt.Println("Before merge sorting:", arr)
+	arr = mergeSort(arr)
+	fmt.Println("After merge sorting:", arr)
 }
 
 func insertionSort(arr []int) {
@@ -61,4 +66,54 @@ func heapSort(arr []int) {
 		arr[0], arr[i] = arr[i], arr[0]
 		heapify(arr, i, 0)
 	}
+}
+
+func mergeSort(arr []int) []int {
+	n := len(arr)
+
+	// 如果陣列只有一個元素，則返回該元素
+	if n <= 1 {
+		return arr
+	}
+
+	// 將陣列拆分為兩個子陣列，然後分別對它們進行排序
+	mid := n / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+
+	// 合併排序後的兩個子陣列
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	nL := len(left)
+	nR := len(right)
+	result := make([]int, nL+nR)
+
+	// 將兩個已排序的子陣列合併成一個已排序的陣列
+	i, j, k := 0, 0, 0
+	for i < nL && j < nR {
+		if left[i] < right[j] {
+			result[k] = left[i]
+			i++
+		} else {
+			result[k] = right[j]
+			j++
+		}
+		k++
+	}
+
+	// 將剩餘的元素添加到已排序的陣列中
+	for i < nL {
+		result[k] = left[i]
+		i++
+		k++
+	}
+	for j < nR {
+		result[k] = right[j]
+		j++
+		k++
+	}
+
+	return result
 }
